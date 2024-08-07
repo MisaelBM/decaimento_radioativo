5bfc0a121d2f162c17b574d781d58f96fdc8e856
MisaelBM <morgadoandreia84misael@gmail.com>
2024-08-07 18:55:40 -0300

    Commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById("bntSoftware").addEventListener("click", () => {
-    open("https://misaelbm.github.io/decaimento_radioativo/software.html");
-});
+document.getElementById("btnSoftware").addEventListener('click', () => open("https://misaelbm.github.io/decaimento_radioativo/software.html"));
+document.getElementById("btnConfig").addEventListener('click', () => document.getElementById("configContent").style.display = "flex");
+document.getElementById("exitConfig").addEventListener('click', () => document.getElementById("configContent").style.display = "none");
+document.getElementById("effectsSounds").checked = localStorage.getItem("effectsSoundsRes") == undefined ? true : JSON.parse(localStorage.getItem("effectsSoundsRes"));
+document.getElementById("effectsSoundsButton").addEventListener('click', () => localStorage.setItem("effectsSoundsRes", `${!document.getElementById("effectsSounds").checked}`));
+document.getElementById("questMode").checked = localStorage.getItem("questModeRes") == undefined ? false : JSON.parse(localStorage.getItem("questModeRes"));
+document.getElementById("questModeButton").addEventListener('click', () => localStorage.setItem("questModeRes", `${!document.getElementById("questMode").checked}`));
